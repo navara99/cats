0d86fc5247b4aeb9ffcc7ba87d3afc4b2eab643c
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 21:00:56 +0000

    handle no file edge case

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
-const breedDetails = require('./syncBreeds');
 
-const displayData = (data)=> {
+const displayData = (data) => {
   console.log('Return Value: ', data);
-}
+};
 
 const breedDetailsFromFile = function(breed, callback) {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
     if (!error) return callback(data);
+    callback(undefined);
   });
 };
 
-breedDetailsFromFile('Bombay', displayData);
+breedDetailsFromFile('Bombaydffg', displayData);
 
 module.exports = breedDetailsFromFile;
