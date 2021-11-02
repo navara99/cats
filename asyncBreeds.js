d14fb4c17e7d7a709306838b3f690f7f8c152214
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 19:54:59 +0000

    implement async solution

---
+++ asyncBreeds.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+
+const displayData = (data)=> {
+  console.log('Return Value: ', data);
+}
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) return callback(data);
+  });
+};
+
+breedDetailsFromFile('Bombay', displayData);
